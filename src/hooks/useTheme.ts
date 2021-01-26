8aa4a6425968e8ac85ab9b2564d4e689e27b4e4f
baptistemcn <baptistemcn@icloud.com>
2021-01-26 21:57:08 +0100

    useTheme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,8 @@
+import {useContext} from "react";
+import {BaseTheme} from "../types";
+
+import {ThemeContext} from "../context";
+
+const useTheme = <Theme extends BaseTheme = BaseTheme>() => useContext(ThemeContext) as Theme;
+
+export default useTheme;
